f3d5381e09a7a8c7055bad2f0131c1dd3b148995
Heraldo Candido da Cruz <hccruz@gmail.com>
2023-11-28 15:08:53 -0300

    Inicializando repositorio

---
+++ README.md
@@ -0,0 +1,8 @@
+# Aplicativo em Streamlit para Dashboards de Vendas de VideoGames
+
+Este é um aplicativo feito no **Streamlit** para vendas de VideoGames.
+
+É um tutorial do curso de **Desenvolvimento Dashboards e Data Apps com Streamlit [2023]**, do Profº Rodrigo Macedo.
+
+Neste aplicativo foi utilizado um dataset de vendas de VideoGames, baixado do Kaggle, no link: <https://www.kaggle.com/datasets/gregorut/videogamesales>.
+
---
+++ app_games.py
@@ -0,0 +1,35 @@
+# Importar as bibliotecas
+import streamlit as st
+import pandas as pd
+import collections
+import plotly.express as px
+
+
+# Importação/Manipulação dos dados
+def importa_dado():
+    df = pd.read_csv("data/vgsales.csv")
+    df.dropna(inplace=True)
+    df = df[df['Year'] <= 2016]
+    df['Year'] = df['Year'].astype(int)
+    return df
+
+
+df = importa_dado()
+
+# Configuração Inicial
+
+st.set_page_config(page_title="Dashboards Games", layout="wide")
+
+st.markdown("<h1 style='text-align:center;'>📊Dashboards de Games</h1>",
+            unsafe_allow_html=True)
+
+# Remove Estilo Streamlit
+remove_st_estilo = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+    </style>
+"""
+
+st.markdown(remove_st_estilo, unsafe_allow_html=True)
